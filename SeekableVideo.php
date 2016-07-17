aa27bb95a6d26f8d5fc04eaac4fdbc5989f2e60a
Andrea Gardoni <blazeag@gmail.com>
2016-07-17 18:43:10 +0200

    Update SeekableVideo.php

--- SeekableVideo.php
+++ SeekableVideo.php
@@ -20,7 +20,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program;
- * if not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * if not, see <http://www.gnu.org/licenses/lgpl-2.1.html>.
  *****************************************************************************/
 
 class SeekableVideo
